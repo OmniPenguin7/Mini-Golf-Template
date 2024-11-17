31d0c3f0b0d19042ed5cd7e0f5ce1758cbb9fca9
LOIS SHERWOOD <2107802@Swansea.ac.uk>
2024-11-17 22:25:46 +0000

    Version 5 - Fixed LOD on trees - Started work on dungeon - Started work on main bedroom - Tried to fix code for player movement

--- Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement2.cs
+++ Assets/Polytope Studio/Lowpoly_Demos/Environment_Free/Helpers/PlayerMovement2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement2 : MonoBehaviour
+public class PlayerMovement3 : MonoBehaviour
 {
     public CharacterController controller;
 
